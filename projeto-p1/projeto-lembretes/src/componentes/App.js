0ffdd4bfb70482562fa5d80597ca143ad9deccff
João Vitor Serra de Freitas <joao.freitas29@fatec.sp.gov.br>
2024-10-04 16:57:32 -0300

    feat(p1-pdmt): cria componente principal e faz hello

---
+++ projeto-p1/projeto-lembretes/src/componentes/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          Hello, lembretes
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ projeto-p1/projeto-lembretes/src/index.js
@@ -0,0 +1,11 @@
+/*Componentes*/
+import App from './componentes/App'
+/*Bootstrap*/
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
